a80d7f98aa4e48b3bfa5ff782d3d55987e4706f3
Bryan Chen <bryanchen74@gmail.com>
2022-03-04 00:01:51 -0500

    Add content to popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,13 +1,42 @@
 import cn from "classnames";
+import { useEffect, useState } from "react";
+import { START_DATE } from "../store";
+
+function getHoursRemaining() {
+  const now = new Date();
+  const diff = now.getTime() - START_DATE.getTime();
+  const hoursRemaining = 24 - ((diff / 1000 / 60 / 60) % 24);
+  console.log(diff, hoursRemaining);
+  if (hoursRemaining > 0.95) {
+    return hoursRemaining.toFixed(0);
+  } else {
+    return hoursRemaining.toFixed(1);
+  }
+}
 
 type PopupProps = {
   hidden: boolean;
   onClose: () => void;
 };
 export default function Popup(props: PopupProps) {
+  const [hoursRemaining, setHoursRemaining] = useState(getHoursRemaining);
+
+  useEffect(() => {
+    // Update hoursRemaining every time popup is opened
+    if (!props.hidden) {
+      setHoursRemaining(getHoursRemaining);
+    }
+  }, [props.hidden]);
+
   return (
     <div className={cn("popup-wrapper", props.hidden && "hidden")}>
       <div className="popup">
+        <p>Guess all 32 Duodecordle words in 42 tries!</p>
+        <p>
+          A new Daily Duodecordle will be available in {hoursRemaining} hour
+          {hoursRemaining === "1" ? "" : "s"}.
+        </p>
+        <hr className="separator" />
         <p>Duotrigordle by Bryan Chen</p>
         <p>
           Source code on{" "}

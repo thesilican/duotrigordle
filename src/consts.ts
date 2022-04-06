7a1664ca55ab90305affd89b0668f9563b861de6
Bryan Chen <bryanchen74@gmail.com>
2022-04-06 00:50:26 -0400

    Fix minor bug with start date timezone

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,13 @@ export const NUM_BOARDS = 32;
 
 export const NUM_GUESSES = 37;
 
-export const START_DATE = new Date("2022-03-03T00:00:00");
+export const START_DATE = (() => {
+  // Use this method so that the start date is offset by current timezone offset
+  // Old method had problems with the start date being before DST
+  const utcDate = new Date("2022-03-03T00:00:00Z").getTime();
+  const offset = new Date().getTimezoneOffset();
+  return new Date(utcDate + offset * 60 * 1000);
+})();
 
 export const WORDS_TARGET = [
   "ABACK",

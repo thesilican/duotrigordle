c85a0b4fc7d1c0c8c4ce46d5f69658a117308b74
Bryan Chen <bryanchen74@gmail.com>
2022-08-26 03:15:46 -0400

    Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import "./index.css";
 import { store } from "./store";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

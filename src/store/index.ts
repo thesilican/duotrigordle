183acd9e3f80c443b7a9b17650a9123605e4ce71
Bryan Chen <bryanchen74@gmail.com>
2022-08-25 21:45:08 -0400

    Refactor redux slices Each slice now has reference to the full root state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Reducer } from "@reduxjs/toolkit";
 import {
   TypedUseSelectorHook,
   useSelector as useSelectorOriginal,
 } from "react-redux";
-import { gameReducer } from "./game";
-import { popupsReducer } from "./popups";
-import { settingsReducer } from "./settings";
-import { statsReducer } from "./stats";
+import { gameInitialState, gameReducer, GameState } from "./slice/game";
+import { PopupsState, popupsInitialState, popupsReducer } from "./slice/popups";
+import {
+  SettingsState,
+  settingsInitialState,
+  settingsReducer,
+} from "./slice/settings";
+import { StatsState, statsInitialState, statsReducer } from "./slice/stats";
+
+export type RootState = {
+  game: GameState;
+  settings: SettingsState;
+  popups: PopupsState;
+  stats: StatsState;
+};
+export const initialState: RootState = {
+  game: gameInitialState,
+  settings: settingsInitialState,
+  popups: popupsInitialState,
+  stats: statsInitialState,
+};
+
+// Create root reducer by reducing reducers
+// (I don't really want to use https://github.com/redux-utilities/reduce-reducers)
+const reducers: Reducer[] = [
+  gameReducer,
+  settingsReducer,
+  popupsReducer,
+  statsReducer,
+];
 
 export const store = configureStore({
-  reducer: {
-    game: gameReducer,
-    settings: settingsReducer,
-    popups: popupsReducer,
-    stats: statsReducer,
-  },
+  reducer: (state, action) => reducers.reduce((s, r) => r(s, action), state),
 });
-export type RootState = ReturnType<typeof store.getState>;
 
 // Partially monomorphise useSelector with State
 export const useSelector: TypedUseSelectorHook<RootState> = useSelectorOriginal;
 
 // Reexports
-export * from "./game";
-export * from "./popups";
-export * from "./settings";
-export * from "./stats";
+export * from "./slice/game";
+export * from "./slice/popups";
+export * from "./slice/settings";
+export * from "./slice/stats";
---
+++ src/store/slice/game.ts
@@ -0,0 +1,96 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+import { initialState } from "..";
+import { NUM_BOARDS, NUM_GUESSES, WORDS_VALID } from "../../consts";
+import { allWordsGuessed, getTargetWords, range } from "../../funcs";
+
+export type GameState = {
+  // Daily duotrigordle number (seed for target words)
+  id: number;
+  // Current word input
+  input: string;
+  // List of guesses
+  guesses: string[];
+  // 32 wordle targets
+  targets: string[];
+  // Whether or not the game is finished
+  gameOver: boolean;
+  // Whether or not the game is in practice mode
+  practice: boolean;
+  // Start timestamp (milliseconds from unix epoch)
+  startTime: number;
+  // End timestamp (milliseconds from unix epoch)
+  endTime: number;
+};
+export const gameInitialState: GameState = {
+  id: 0,
+  input: "",
+  guesses: [],
+  targets: range(NUM_BOARDS).map((_) => "AAAAA"),
+  gameOver: false,
+  practice: true,
+  startTime: 0,
+  endTime: 0,
+};
+
+export const loadGame = createAction<{ game: GameState }>("game/loadGame");
+export const startGame = createAction<{ id: number; practice: boolean }>(
+  "game/startGame"
+);
+export const inputLetter = createAction<{ letter: string }>("game/inputLetter");
+export const inputBackspace = createAction("game/inputBackspace");
+export const inputEnter = createAction<{ timestamp: number }>(
+  "game/inputEnter"
+);
+
+export const gameReducer = createReducer(
+  () => initialState,
+  (builder) =>
+    builder
+      .addCase(loadGame, (state, action) => {
+        state.game = action.payload.game;
+      })
+      .addCase(startGame, (state, action) => {
+        state.game = {
+          id: action.payload.id,
+          targets: getTargetWords(action.payload.id),
+          guesses: [],
+          input: "",
+          gameOver: false,
+          practice: action.payload.practice,
+          startTime: 0,
+          endTime: 0,
+        };
+      })
+      .addCase(inputLetter, (state, action) => {
+        if (state.game.gameOver) return;
+        if (state.game.input.length < 5) {
+          state.game.input += action.payload.letter;
+        }
+      })
+      .addCase(inputBackspace, (state, _) => {
+        if (state.game.gameOver) return;
+      })
+      .addCase(inputEnter, (state, action) => {
+        const game = state.game;
+        if (game.gameOver) return;
+
+        const guess = game.input;
+        game.input = "";
+        if (!WORDS_VALID.has(guess)) {
+          return;
+        }
+        game.guesses.push(guess);
+        // Start timer on first guess
+        if (game.guesses.length === 1) {
+          game.startTime = action.payload.timestamp;
+        }
+
+        if (
+          game.guesses.length === NUM_GUESSES ||
+          allWordsGuessed(game.guesses, game.targets)
+        ) {
+          game.gameOver = true;
+          game.endTime = action.payload.timestamp;
+        }
+      })
+);

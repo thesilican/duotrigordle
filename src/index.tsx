a3d9ff0c802bb1fb1ce72300d7442c05d86837df
Bryan Chen <bryanchen74@gmail.com>
2022-12-23 23:55:36 -0500

    Add React strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./components/App";
@@ -6,9 +7,9 @@ import { store } from "./store";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );

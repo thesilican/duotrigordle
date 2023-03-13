a6d64f4ec8db506212d2ec586c6d716c8b37467f
Bryan Chen <bryanchen74@gmail.com>
2023-03-13 10:26:54 -0400

    Revert ezoic ads

--- src/components/AdBox/AdBox.tsx
+++ src/components/AdBox/AdBox.tsx
@@ -6,10 +6,6 @@ import styles from "./AdBox.module.css";
 // If you're develping Duotrigordle locally and would like to hide ads, set this to true
 const overrideHideAds = false;
 
-declare global {
-  const ezstandalone: any;
-}
-
 export function AdBox() {
   const hideAds = useAppSelector((s) => s.settings.hideAds);
   const [loaded, setLoaded] = useState(false);
@@ -17,16 +13,10 @@ export function AdBox() {
   useEffect(() => {
     if (!loaded && !hideAds && !overrideHideAds) {
       setTimeout(() => {
-        // Load Ezoic ads
+        // Load Google Adsense
         try {
           // @ts-ignore
-          window.ezstandalone = window.ezstandalone || {};
-          ezstandalone.cmd = ezstandalone.cmd || [];
-          ezstandalone.cmd.push(function () {
-            ezstandalone.define(101);
-            ezstandalone.enable();
-            ezstandalone.display();
-          });
+          (adsbygoogle = window.adsbygoogle || []).push({});
         } catch {}
       }, 1000);
       setLoaded(true);
@@ -39,7 +29,16 @@ export function AdBox() {
 
   return (
     <div className={cn("ads-box", styles.adBox)}>
-      <div id="ezoic-pub-ad-placeholder-101"> </div>
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: "inline-block",
+          width: "min(100vw, 600px)",
+          height: "60px",
+        }}
+        data-ad-client="ca-pub-4459769759726497"
+        data-ad-slot="5019524864"
+      />
     </div>
   );
 }
